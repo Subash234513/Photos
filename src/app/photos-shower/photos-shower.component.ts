714400e51c86fdd576e2eaaa2bf4fb5033f572bb
Subash234513 <130580906+Subash234513@users.noreply.github.com>
2024-03-23 22:28:12 +0530

    new

--- src/app/photos-shower/photos-shower.component.ts
+++ src/app/photos-shower/photos-shower.component.ts
@@ -12,7 +12,7 @@ export class PhotosShowerComponent implements OnInit {
   ngOnInit(): void {
   }
   data:any='../../assets/wallpaper1.jpeg'
-  List=['./assets/wallpaper1.jpeg','./assets/wallpaper2.jpeg','./assets/wallpaper3.jpeg','./assets/wallpaper4.jpeg','./assets/wallpaper5.jpeg','./assets/wallpaper6.jpeg','./assets/wallpaper7.jpeg','./assets/wallpaper8.jpeg','./assets/wallpaper9.jpeg','./assets/wallpaper10.jpeg','./assets/wallpaper11.jpeg','./assets/wallpaper12.jpeg','./assets/wallpaper13.jpeg','./assets/wallpaper14.jpeg']
+  List=['/assets/wallpaper1.jpeg','/assets/wallpaper2.jpeg','/assets/wallpaper3.jpeg','/assets/wallpaper4.jpeg','/assets/wallpaper5.jpeg','/assets/wallpaper6.jpeg','/assets/wallpaper7.jpeg','/assets/wallpaper8.jpeg','/assets/wallpaper9.jpeg','/assets/wallpaper10.jpeg','/assets/wallpaper11.jpeg','/assets/wallpaper12.jpeg','/assets/wallpaper13.jpeg','/assets/wallpaper14.jpeg']
   Image(data){
     this.data=data
   }
